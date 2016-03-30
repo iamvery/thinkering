fc34f4624ea0ef81da7d0f8cba9cda3104be42f7
Jay Hayes <ur@iamvery.com>
2016-03-30 11:14:54 -0500

    Implement stupid in-memory model for thunks

---
+++ app/lib/thunk.rb
@@ -0,0 +1,30 @@
+class Thunk
+  THUNKS = []
+
+  def self.all
+    THUNKS.map(&method(:new))
+  end
+
+  def self.create(*args)
+    new(*args).save
+  end
+
+  attr_reader :body, :url
+
+  def initialize(attributes = Hash.new)
+    @body = attributes.fetch("body") { "" }
+    @url = attributes.fetch("url") { "" }
+  end
+
+  def save
+    THUNKS << to_h
+    self
+  end
+
+  def to_h
+    {
+      "body" => body,
+      "url" => url,
+    }
+  end
+end
