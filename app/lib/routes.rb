8b2c79d5e7a0a0a1ebc0d30763e04bb60b80ec59
Jay Hayes <ur@iamvery.com>
2016-03-30 12:50:22 -0500

    Bind new model to form
    This fills out the form inputs' names. The names are scoped to the
    form's scope. So in this case it's "form", so inputs become "form[body]"
    for example.

--- app/lib/routes.rb
+++ app/lib/routes.rb
@@ -2,6 +2,7 @@ Pakyow::App.routes do
   default do
     Thunk.create("body" => "lol", "url" => "http://iamvery.com")
     view.scope(:thunk).apply(Thunk.all)
+    view.scope(:form).bind(Thunk.new.to_h)
   end
 
   # define application routes here
