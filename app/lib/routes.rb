32b758fa032fa6a051467fdf1d94d5d0c198159c
Jay Hayes <ur@iamvery.com>
2016-03-30 13:00:43 -0500

    No need to create one anymore; we can do that ourselves!

--- app/lib/routes.rb
+++ app/lib/routes.rb
@@ -1,6 +1,5 @@
 Pakyow::App.routes do
   default do
-    Thunk.create("body" => "lol", "url" => "http://iamvery.com")
     view.scope(:thunk).apply(Thunk.all)
     view.scope(:form).bind(Thunk.new.to_h)
   end
